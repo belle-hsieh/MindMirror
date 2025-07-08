74659669bbfb10f1ede266159d1b29f4fa74c0f8
belle hsieh <hsiehannabelle2024@gmail.com>
2025-07-07 17:30:36 -0700

    theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
+import CornerImage from '../components/ui/CornerImage';
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
-      <main className="centered-content">
+      {/* Corner Images */}
+      <div className="fixed inset-0 z-0">
+        <CornerImage
+          position="top-left"
+          src="/corner-top-left.png"
+          alt="Top-left corner decoration"
+        />
+        <CornerImage
+          position="top-right"
+          src="/corner-top-right.png"
+          alt="Top-right corner decoration"
+        />
+        <CornerImage
+          position="bottom-left"
+          src="/corner-bottom-left.png"
+          alt="Bottom-left corner decoration"
+        />
+        <CornerImage
+          position="bottom-right"
+          src="/corner-bottom-right.png"
+          alt="Bottom-right corner decoration"
+        />
+      </div>
+      
+      <main className="relative z-10 centered-content">
         <div className="title-container">
           <h1 className="main-title">
             MindMirror:
---
+++ src/components/ui/CornerImage.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import React from 'react';
+
+interface CornerImageProps {
+  position: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
+  src: string;
+  alt: string;
+  size?: number;
+  opacity?: number;
+}
+
+const CornerImage: React.FC<CornerImageProps> = ({
+  position,
+  src,
+  alt,
+  size = 600,
+  opacity = 0.3,
+}) => {
+  const styles = {
+    position: 'fixed' as const,
+    zIndex: 0, // Move above background but below content
+    width: `${size}px`,
+    height: `${size}px`,
+    opacity,
+  };
+
+  const positionStyles = {
+    'top-left': { top: -80, left: -80 },
+    'top-right': { top: -80, right: -80 },
+    'bottom-left': { bottom: -80, left: -80 },
+    'bottom-right': { bottom: -80, right: -80 },
+  };
+
+  return (
+    <div 
+      style={{ ...styles, ...positionStyles[position] }}
+      className="relative"
+    >
+      <img
+        src={src}
+        alt={alt}
+        className="absolute inset-0 object-contain w-full h-full"
+      />
+    </div>
+  );
+};
+
+export default CornerImage;
